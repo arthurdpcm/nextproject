6c4916520e0a3370467f97455f60264b06536d2f
arthurdpcm <duartearthur42@gmail.com>
2022-04-15 05:28:52 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // env: {
+  //   process.env.bdPath
+  //   bdPath: 'mysql...'
+  // }
+  
 }
 
 module.exports = nextConfig
